e06acb34111b8fb25ce782d264d3694cd0b77277
Konstantin Volodin <volodin.konstantin@gmail.com>
2015-12-13 23:57:55 +0300

    Update sonar.ino

--- sonar.ino
+++ sonar.ino
@@ -1,13 +1,19 @@
 int echoPin = 9; 
 int trigPin = 8; 
+int modePin = 3;
+int loopLimitPin = 4;
 
 unsigned char loopCounter = 0;
 double avgCm = 0.0;
+
+int loopLimit = 5;
 
 void setup() { 
   Serial.begin (9600); 
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
+  pinMode(modePin, INPUT);
+  pinMode(loopLimitPin, INPUT);
 } 
 
 void loop() { 
@@ -22,11 +28,21 @@ void loop() {
   cm = duration / 5.8;
   //Serial.println(cm); 
   //Serial.println(" cm"); 
-  delay(100);
+  
+  if (digitalRead(modePin) == 1)
+    delay(500);
+  else
+    delay(100);
 
-  if (loopCounter == 10)
+  if (digitalRead(loopLimitPin) == 1)
+    loopLimit = 9;
+  else
+    loopLimit = 4;
+
+  
+  if (loopCounter == loopLimit)
   {
-     Serial.println(avgCm / 10);
+     Serial.print(avgCm / loopLimit); Serial.print(" "); Serial.println(micros());
      avgCm = 0;
      loopCounter = 0; 
   }
